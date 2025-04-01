0ff7a131bb65b822a8bf838dddf9201c1bf2265b
Prashant Tripathi <89456446+5138prash@users.noreply.github.com>
2025-04-01 19:03:05 +0530

    Update Home.jsx

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,114 +1,102 @@
 import React from "react";
-import Carousel from "../components/carousel/Carousel";
-import Favourite_Brands from "../data/Favourite_Brands";
-import ShopByCategory from "../data/ShopByCategory";
-import Grand_Global_Brands from "../data/Grand_Global_Brands";
-import { Pagination } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay } from "swiper/modules";
-import { useNavigate } from "react-router-dom";
+import Carousel from "../components/carousel/Carousel";  // Carousel component
+import Favourite_Brands from "../data/Favourite_Brands";  // Favourite brands data
+import ShopByCategory from "../data/ShopByCategory";  // Shop by category data
+import Grand_Global_Brands from "../data/Grand_Global_Brands";  // Grand global brands data
+import { Pagination } from "swiper/modules";  // Pagination module from Swiper
+import { Swiper, SwiperSlide } from "swiper/react";  // Swiper and SwiperSlide components
+import { Autoplay } from "swiper/modules";  // Autoplay module from Swiper
+import { useNavigate } from "react-router-dom";  // useNavigate hook for navigation
 
 // Import Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
 
 const Home = () => {
-  const navigate = useNavigate();
+  const navigate = useNavigate();  // For page navigation
 
   return (
     <section className="home-section">
-      <Carousel />
+      <Carousel />  {/* Carousel component */}
 
-      <div className=" my-5 p-8 heading">
-        <h4 className="px-[50px]  text-[38px] font-[650] text-gray-700  uppercase tracking-widest">
+      {/* Favourite Brands Section */}
+      <div className="my-5 p-8 heading">
+        <h4 className="px-[50px] text-[38px] font-[650] text-gray-700 uppercase tracking-widest">
           Favourite Brands
         </h4>
       </div>
 
-      <div className="slides py-4 flex  w-full ">
-        {/* Brands */}
-        <div className="flex gap-1 w-full justify-around flex-wrap slide-1 ">
-          {Favourite_Brands.map(({ id, image }) => {
-            return (
-              <div className="slides-img cursor-pointer" key={id}>
-                <img
-                  src={image}
-                  onClick={() => navigate("/products")}
-                  alt=""
-                  className="w-[205px]"
-                />
-              </div>
-            );
-          })}
+      <div className="slides py-4 flex w-full">
+        <div className="flex gap-1 w-full justify-around flex-wrap slide-1">
+          {Favourite_Brands.map(({ id, image }) => (
+            <div className="slides-img cursor-pointer" key={id}>
+              <img
+                src={image}
+                onClick={() => navigate("/products")}  // Navigate to products page on image click
+                alt=""
+                className="w-[205px]"
+              />
+            </div>
+          ))}
         </div>
       </div>
 
-      <div className=" heading my-5 p-8 ">
-        <h4 className=" px-[50px] text-[38px] font-[650] text-gray-700  uppercase tracking-widest">
-          {" "}
+      {/* Grand Global Brands Section */}
+      <div className="heading my-5 p-8">
+        <h4 className="px-[50px] text-[38px] font-[650] text-gray-700 uppercase tracking-widest">
           Grand Global Brands
         </h4>
       </div>
 
-      <div className="slides-2 my-10 h-[440px] w-full flex ">
+      <div className="slides-2 my-10 h-[440px] w-full flex">
         <Swiper
-          className="flex swiper-2 py-8 "
-          modules={[Pagination, Autoplay]}
+          className="flex swiper-2 py-8"
+          modules={[Pagination, Autoplay]}  // Swiper modules
           spaceBetween={0}
           slidesPerView={6}
           slidesPerGroup={6}
           loop={true}
           grabCursor={true}
           pagination={{ clickable: true }}
-          autoplay={{ delay: 2010, disableOnInteraction: false }}
+          autoplay={{ delay: 2010, disableOnInteraction: false }}  // Autoplay configuration
           breakpoints={{
-            200: { slidesPerView: 2, slidesPerGroup: 2 }, 
-            600: { slidesPerView: 3, slidesPerGroup: 3 }, 
-            
-            1430: { slidesPerView: 6, slidesPerGroup: 6 }, // Default settings for above 1430px
+            200: { slidesPerView: 2, slidesPerGroup: 2 },
+            600: { slidesPerView: 3, slidesPerGroup: 3 },
+            1430: { slidesPerView: 6, slidesPerGroup: 6 },
           }}
         >
-          {Grand_Global_Brands.map(({ id, image }) => {
-            return (
-              <SwiperSlide className="cursor-pointer h-[400px]" key={id}>
-                <img
-                  className="swiper-img w-full h-full object-contain"
-                  src={image}
-                  alt={`image${id}`}
-                  onClick={() => navigate("/products")}
-                />
-              </SwiperSlide>
-            );
-          })}
+          {Grand_Global_Brands.map(({ id, image }) => (
+            <SwiperSlide className="cursor-pointer h-[400px]" key={id}>
+              <img
+                className="swiper-img w-full h-full object-contain"
+                src={image}
+                alt={`image${id}`}
+                onClick={() => navigate("/products")}  // Navigate to products page
+              />
+            </SwiperSlide>
+          ))}
         </Swiper>
       </div>
 
-      <div className="heading category-heading my-5 p-8 ">
-        <h4 className="px-[50px] text-[38px] font-[650] text-gray-700  uppercase tracking-widest">
-          {" "}
-          Shop by category
+      {/* Shop by Category Section */}
+      <div className="heading category-heading my-5 p-8">
+        <h4 className="px-[50px] text-[38px] font-[650] text-gray-700 uppercase tracking-widest">
+          Shop by Category
         </h4>
       </div>
 
-      <div className="shopByCategory w-full px-2 py-10 ">
-        <div>
-          <div className="category-items  w-full px-2 gap-7 flex  flex-wrap items-center justify-around ">
-            {ShopByCategory.map(({ id, image }) => {
-              return (
-                <div
-                  key={id}
-                  className="category-img  w-[250px] p-2  cursor-pointer flex"
-                >
-                  <img
-                    src={image}
-                    onClick={() => navigate("/products")}
-                    alt=""
-                    className=" w-full h-full object-fit"
-                  />
-                </div>
-              );
-            })}
-          </div>
+      <div className="shopByCategory w-full px-2 py-10">
+        <div className="category-items w-full px-2 gap-7 flex flex-wrap items-center justify-around">
+          {ShopByCategory.map(({ id, image }) => (
+            <div key={id} className="category-img w-[250px] p-2 cursor-pointer flex">
+              <img
+                src={image}
+                onClick={() => navigate("/products")}  // Navigate to products page
+                alt=""
+                className="w-full h-full object-fit"
+              />
+            </div>
+          ))}
         </div>
       </div>
     </section>
