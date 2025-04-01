81a81de272d8c82a3423b3bd9a82b98e00bfcf28
Prashant Tripathi <89456446+5138prash@users.noreply.github.com>
2025-04-01 19:02:01 +0530

    Update Bag.jsx

--- src/pages/Bag.jsx
+++ src/pages/Bag.jsx
@@ -1,17 +1,17 @@
 import React from "react";
+import AddToCart from "../components/addToCart/AddToCart";  // Importing the component to display cart items
+import CartEmpty from "../components/addToCart/CartEmpty";  // Importing the component to display when the cart is empty
+import { useSelector } from "react-redux";  // Importing useSelector to access the state from Redux
 
-import AddToCart from "../components/addToCart/AddToCart";
-import CartEmpty from "../components/addToCart/CartEmpty";
-import { useSelector } from "react-redux";
 const Bag = () => {
-  let cart = useSelector(state => state.product.cart)
+  // Getting the cart data from the Redux store
+  let cart = useSelector(state => state.product.cart);
 
   return (
     <section className="mt-[100px] w-[980px] w-full flex justify-center border-gray-200 bag-section">
-     
-     
-    {cart.length === 0 ? <CartEmpty/> : <AddToCart/>}
-      
+      {/* Checking if the cart is empty */}
+      {cart.length === 0 ? <CartEmpty /> : <AddToCart />} 
+      {/* If the cart is empty, display the CartEmpty component, otherwise display the AddToCart component */}
     </section>
   );
 };
