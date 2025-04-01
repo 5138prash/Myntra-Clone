ecc40c1b5ec2f993670de32baa615cb93dec5f3f
Prashant Tripathi <89456446+5138prash@users.noreply.github.com>
2025-04-01 18:55:56 +0530

    Update Carousel.jsx

--- src/components/carousel/Carousel.jsx
+++ src/components/carousel/Carousel.jsx
@@ -1,33 +1,35 @@
 import React from "react";
-import Data from "../../data/Carousel_Data"
-import { Pagination } from "swiper/modules";
-import { Swiper, SwiperSlide } from "swiper/react";
-import { Autoplay } from "swiper/modules";
+import Data from "../../data/Carousel_Data"; // Importing carousel data
+import { Pagination, Autoplay } from "swiper/modules"; // Importing Swiper modules
+import { Swiper, SwiperSlide } from "swiper/react"; // Importing Swiper components
 
-// Import Swiper styles
+// Importing Swiper styles
 import "swiper/css";
 import "swiper/css/pagination";
+
 const Carousel = () => {
   return (
     <section>
-      <div className=" main-carousel mt-[120px]  h-[540px] flex ">
+      {/* Main Carousel Container */}
+      <div className="main-carousel mt-[120px] h-[540px] flex">
         <Swiper
-          modules={[Pagination, Autoplay]}
-          spaceBetween={10}
-          slidesPerView={1}
-          loop={true}
-          grabCursor={true}
-          pagination={{ clickable: true }}
-          autoplay={{delay: 2500, disableOnInteraction: false}}
-          
+          modules={[Pagination, Autoplay]} // Enabling pagination and autoplay
+          spaceBetween={10} // Space between slides
+          slidesPerView={1} // Show only one slide at a time
+          loop={true} // Enable infinite loop
+          grabCursor={true} // Change cursor to grab when hovering
+          pagination={{ clickable: true }} // Enable pagination bullets
+          autoplay={{ delay: 2500, disableOnInteraction: false }} // Autoplay settings
         >
+          {/* Mapping through carousel data to create slides */}
           {Data.map(({ id, url }) => {
             return (
-                <SwiperSlide key={id}>
-                    <div>
-                    <img src={url} alt={`Image${id}`} />
-                    </div>
-                </SwiperSlide>
+              <SwiperSlide key={id}>
+                <div>
+                  {/* Image for each slide */}
+                  <img src={url} alt={`Image${id}`} />
+                </div>
+              </SwiperSlide>
             );
           })}
         </Swiper>
