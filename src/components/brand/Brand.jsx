f76192f0ea0e6658ef4ec39bae93daf014726ecc
Prashant Tripathi <89456446+5138prash@users.noreply.github.com>
2025-04-01 18:53:09 +0530

    Update Brand.jsx

--- src/components/brand/Brand.jsx
+++ src/components/brand/Brand.jsx
@@ -3,26 +3,28 @@ import React from 'react'
 const Brand = () => {
   return (
     <>
-      <div className=' my-5 p-8'>
-        <h4 className='px-[50px]  text-[38px] font-[650] text-gray-700  uppercase tracking-widest'>Favourite Brands</h4>
-        </div>
+      {/* Section Title */}
+      <div className='my-5 p-8'>
+        <h4 className='px-[50px] text-[38px] font-[650] text-gray-700 uppercase tracking-widest'>Favourite Brands</h4>
+      </div>
 
-        <div className=' py-7  h-[220px] flex  items-center justify-center'>
-        {/* Brands */}
-        <div className='flex '> 
-        {
-            Favourite_Brands.map(({id, image})=>{
-                return (
-                    <div className='cursor-pointer' key={id}>
-                        <img src={image} alt="" className='w-[210px]'/>
-                    </div>
-                )
+      {/* Brands Display Section */}
+      <div className='py-7 h-[220px] flex items-center justify-center'>
+        <div className='flex'> 
+          {
+            // Mapping over the Favourite_Brands array to display each brand's image
+            Favourite_Brands.map(({ id, image }) => {
+              return (
+                <div className='cursor-pointer' key={id}>
+                  <img src={image} alt="Brand" className='w-[210px]' />
+                </div>
+              )
             })
-        }
+          }
         </div>
-        </div>
+      </div>
     </>
   )
 }
 
-export default Brand
+export default Brand;
